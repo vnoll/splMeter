12814a07529d9065bd412509510f3512e3bd3f22
Jakub Bakowski <jakub.bakowski111@gmail.com>
2022-03-20 19:27:15 +0100

    CMake
    Signed-off-by: Jakub Bakowski <jakub.bakowski111@gmail.com>

--- spl.cpp
+++ spl.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <SPLConfig.h>
 
 int main(int argv, char* argc[]) {
     
-    std::cout << "Hello world!" << std::endl;
+    std::cout << "Version: " << SoundPressureLevel_VERSION_MAJOR << "." << SoundPressureLevel_VERSION_MINOR << "." << SoundPressureLevel_VERSION_PATCH << std::endl;
     return 1;
 }
