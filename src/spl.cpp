96ea952f2bde358f220dee48e989d4f3597ff377
bakowski-jakub <55808307+bakowski-jakub@users.noreply.github.com>
2022-04-02 14:39:12 +0200

    Clean up.

--- src/spl.cpp
+++ src/spl.cpp
@@ -1,12 +1,17 @@
 // audioprobe.cpp
 #include <iostream>
 #include "RtAudio.h"
+#include "all.hpp"
 
 #define __LINUX_ALSA__
 #define __OS_LINUX__
-
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
 int record(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
            double streamTime, RtAudioStreamStatus status, void *userData) {
+    auto t1 = high_resolution_clock::now();
     int32_t *iBuf = (int32_t*) inputBuffer;
     if(status)
         std::cout << "Stream overflow detected!" << std::endl;
@@ -16,9 +21,11 @@ int record(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
         std::cout << "Index: " << i << " :" << iBuf[i] << std::endl;
     }
     std::cout << "######" << std::endl;
+    auto t2 = high_resolution_clock::now();
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+    std::cout << ms_int.count() << "ms\n";
     return 0;
 }
-
 
 int main()
 {
