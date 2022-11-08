bdff99b130834e82851532643b8ae37d3cb3b8c2
bakowski-jakub <55808307+bakowski-jakub@users.noreply.github.com>
2022-11-08 08:50:36 +0100

    Update biquad.cpp

--- src/biquad.cpp
+++ src/biquad.cpp
@@ -1,7 +1,7 @@
 #include "../inc/biquad.h"
 
 double Biquad::filter(double in) {
-    double out = z1 + b0* in;
+    double out = z1 + b0 * in;
     z1 = z2 + b1 * in - a1 * out;
     z2 = b2 * in - a2 * out;
     return out;
